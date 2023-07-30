270849841a81b472abd46af55459a79d684951c9
gen <jigk@ma.com>
2023-07-30 21:57:26 +0300

    Added lutlets and logic

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private var colorView: UIView!
+    
+    @IBOutlet private var redSliderValue: UILabel!
+    @IBOutlet private var greenSliderValue: UILabel!
+    @IBOutlet private var blueSliderValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
     }
-
-
+    
+    private func updateColorOfView() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        redSliderValue.text = String(format: "%.2f", sender.value)
+        updateColorOfView()
+    }
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+        greenSliderValue.text = String(format: "%.2f", sender.value)
+        updateColorOfView()
+    }
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+        blueSliderValue.text = String(format: "%.2f", sender.value)
+        updateColorOfView()
+    }
+    
 }
 
